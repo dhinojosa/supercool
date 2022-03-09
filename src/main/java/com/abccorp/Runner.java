b3f5125fa983460d6e6909dda90c39dfd8c0fcab
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-03-09 15:35:01 -0700

    initial commit of java application

---
+++ src/main/java/com/abccorp/Runner.java
@@ -0,0 +1,30 @@
+package com.abccorp;
+
+import com.xyzcorp.House;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello there");
+        House h1 = new House(2000, true, 2);
+        System.out.println(h1.getSquareFoot());
+
+        House h2 = new House(4000, true, 3);
+        System.out.println(h2.getSquareFoot());
+
+        House h3 = h2;
+        System.out.println(h3.getSquareFoot());
+
+        com.xyzcorp.Person p = new com.xyzcorp.Person("Jesus", "Fernandez");
+        System.out.println(p.getFirstName());
+        System.out.println(p.getLastName());
+
+        com.xyzcorp.Person p2 = new com.xyzcorp.Person("Jesus", "Fernandez");
+
+        System.out.println(p == p2);      //reference
+        System.out.println(p.equals(p2)); //object equality
+
+        System.out.println(p.hashCode());
+        System.out.println(p2.hashCode());
+
+    }
+}
---
+++ src/main/java/com/xyzcorp/House.java
@@ -0,0 +1,28 @@
+package com.xyzcorp;
+
+public class House {
+    //member variables
+    private int squareFoot;
+    private boolean hasSolar;
+    private int numberGarages;
+
+    public House(int squareFoot, boolean hasSolar, int numberGarages) {
+        this.squareFoot = squareFoot;
+        this.hasSolar = hasSolar;
+        this.numberGarages = numberGarages;
+    }
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public boolean hasSolar() {
+        return hasSolar;
+    }
+
+    public int getNumberGarages() {
+        return numberGarages;
+    }
+}
+
+
+
